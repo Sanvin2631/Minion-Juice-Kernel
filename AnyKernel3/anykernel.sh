f86f19ad6aba71244f26b31371c4edd76756508b
Harikumar <62538135+phoenix-1708@users.noreply.github.com>
2021-08-15 10:31:30 +0530

    Update anykernel.sh

--- AnyKernel3/anykernel.sh
+++ AnyKernel3/anykernel.sh
@@ -4,8 +4,8 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Chaeyoung Kernel
-maintainer.string=dlwlrma123
+kernel.string=PhoenixKernel
+maintainer.string=phoenix-1708
 do.devicecheck=0
 do.modules=0
 do.systemless=1
